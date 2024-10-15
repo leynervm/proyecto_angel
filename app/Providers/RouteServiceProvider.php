d672ef9d2e545f148ee599944cfb928aa96b6d8f
leynervega <leynervega0413@gmail.com>
2024-10-14 23:57:27 -0500

    Actualizacion poth de ruta home login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/orders';
+    public const HOME = '/dashboard/orders';
 
     /**
      * The controller namespace for the application.
