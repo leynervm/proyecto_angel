c46d1563e567a80f58287e17b405aa61387a3b6c
leynervega <leynervega0413@gmail.com>
2024-12-12 00:51:34 -0500

    Fixed actualizado correctamente

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -1,10 +1,5 @@
 <x-admin-layout>
     <div class="w-full mx-auto p-1 flex flex-col gap-5">
-        <div>
-            <livewire:orders.edit-order :order="$order" />
-        </div>
-        <div>
-            <livewire:orders.trackings-order :order="$order" />
-        </div>
+        <livewire:orders.edit-order :order="$order" />
     </div>
 </x-admin-layout>
