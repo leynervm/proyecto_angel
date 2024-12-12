6400c74a6e896d2dec496073b2b2ff2ad8047ae1
leynervega <leynervega0413@gmail.com>
2024-12-12 00:44:10 -0500

    Actualizacion de los links redes despues de update consulta dni

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -32,24 +32,29 @@
                 <div class="my-3">
                     <ul class="text-white text-xs md:text-sm font-medium">
                         <li class="flex items-center gap-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
-                                fill-rule="evenodd" clip-rule="evenodd" fill="currentColor"
-                                class="block size-5 text-green-600">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill-rule="evenodd"
+                                clip-rule="evenodd" fill="currentColor" class="block size-4 text-white">
                                 <path
                                     d="M3.29404 1.58612C4.25932 0.620833 5.88661 0.884906 6.49711 2.1059L8.5543 6.22028C8.93929 6.99026 8.78838 7.92021 8.17966 8.52893L4.86304 11.8455C6.35673 14.6391 8.6553 16.9376 11.4488 18.4313L14.7654 15.1147C15.3742 14.506 16.3041 14.3551 17.0741 14.7401L21.1885 16.7973C22.4095 17.4078 22.6735 19.035 21.7083 20.0003L20.2751 21.4335C19.7191 21.9895 18.9422 22.3313 18.1058 22.2912C8.86166 21.8476 1.44676 14.4327 1.00319 5.18854C0.963054 4.35216 1.30492 3.57524 1.86089 3.01927L3.29404 1.58612Z" />
                             </svg>
                             996 976 634
                         </li>
                         <li class="flex items-center gap-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
-                                fill-rule="evenodd" clip-rule="evenodd" fill="currentColor"
-                                class="block size-5 text-green-600">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill-rule="evenodd"
+                                clip-rule="evenodd" fill="currentColor" class="block size-4 text-white">
                                 <path
                                     d="M3.29404 1.58612C4.25932 0.620833 5.88661 0.884906 6.49711 2.1059L8.5543 6.22028C8.93929 6.99026 8.78838 7.92021 8.17966 8.52893L4.86304 11.8455C6.35673 14.6391 8.6553 16.9376 11.4488 18.4313L14.7654 15.1147C15.3742 14.506 16.3041 14.3551 17.0741 14.7401L21.1885 16.7973C22.4095 17.4078 22.6735 19.035 21.7083 20.0003L20.2751 21.4335C19.7191 21.9895 18.9422 22.3313 18.1058 22.2912C8.86166 21.8476 1.44676 14.4327 1.00319 5.18854C0.963054 4.35216 1.30492 3.57524 1.86089 3.01927L3.29404 1.58612Z" />
                             </svg>
                             976 120 205
                         </li>
-                        <li>gigantocalin@outlook.es</li>
+                        <li class="flex items-center gap-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                fill-rule="evenodd" clip-rule="evenodd" class="block size-4 text-white">
+                                <path
+                                    d="M3.75 3C2.23122 3 1 4.23122 1 5.75V17.75C1 19.2688 2.23122 20.5 3.75 20.5H19.75C21.2688 20.5 22.5 19.2688 22.5 17.75V5.75C22.5 4.23122 21.2688 3 19.75 3H3.75ZM6.12803 7.10229C5.77024 6.89358 5.311 7.01443 5.10229 7.37222C4.89358 7.73001 5.01443 8.18925 5.37222 8.39796L10.3645 11.3101C11.2207 11.8096 12.2795 11.8096 13.1358 11.3101L18.128 8.39796C18.4858 8.18925 18.6067 7.73001 18.398 7.37222C18.1892 7.01443 17.73 6.89358 17.3722 7.10229L12.38 10.0144C11.9908 10.2415 11.5095 10.2415 11.1203 10.0144L6.12803 7.10229Z" />
+                            </svg>
+                            gigantocalin@outlook.es
+                        </li>
                     </ul>
                 </div>
 
@@ -59,7 +64,7 @@
                 </div>
                 <div class="my-3 w-full flex gap-2 justify-center lg:justify-start">
                     <a class="shadow-lg block font-normal h-8 w-8 rounded-full outline-none focus:outline-none hover:scale-[1.20] hover:rotate-[360deg] transition-transform ease-out duration-500"
-                        type="button">
+                        href="{{ config('services.links.facebook') }}" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" color="currentColor" fill="solid"
                             class="w-full h-full rounded-full p-1 block bg-white text-blue-800" stroke="currentColor"
                             stroke-width="1.5" stroke-linejoin="round">
@@ -67,8 +72,8 @@
                                 d="M6.18182 10.3333C5.20406 10.3333 5 10.5252 5 11.4444V13.1111C5 14.0304 5.20406 14.2222 6.18182 14.2222H8.54545V20.8889C8.54545 21.8081 8.74951 22 9.72727 22H12.0909C13.0687 22 13.2727 21.8081 13.2727 20.8889V14.2222H15.9267C16.6683 14.2222 16.8594 14.0867 17.0631 13.4164L17.5696 11.7497C17.9185 10.6014 17.7035 10.3333 16.4332 10.3333H13.2727V7.55556C13.2727 6.94191 13.8018 6.44444 14.4545 6.44444H17.8182C18.7959 6.44444 19 6.25259 19 5.33333V3.11111C19 2.19185 18.7959 2 17.8182 2H14.4545C11.191 2 8.54545 4.48731 8.54545 7.55556V10.3333H6.18182Z" />
                         </svg>
                     </a>
-                    <a
-                        class="shadow-lg block font-normal h-8 w-8 rounded-full outline-none focus:outline-none hover:scale-[1.20] hover:rotate-[360deg] transition-transform ease-out duration-500">
+                    <a class="shadow-lg block font-normal h-8 w-8 rounded-full outline-none focus:outline-none hover:scale-[1.20] hover:rotate-[360deg] transition-transform ease-out duration-500"
+                        href="{{ config('services.links.instagram') }}" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" color="currentColor" fill="none"
                             class="w-full h-full rounded-full p-1 block bg-white text-blue-800">
                             <path
